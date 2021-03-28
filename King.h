ed2fe3ec6e6438dab0113f9b36a1392c74de465b
Aleksandar Shishkov <74405864+AleksandarShishkov@users.noreply.github.com>
2021-03-28 14:48:11 +0300

    Update King.h

--- King.h
+++ King.h
@@ -1,7 +1,7 @@
 #ifndef KING_H
 #define KING_H
 
-// including header file Piece.h
+// including Piece.h
 #include "Piece.h"
 
 
@@ -10,10 +10,10 @@ class King :public Piece
 
 private:
 
-	// integer to hold the owner
+	// integer to hold the owner set to 0
 	int owner_king = 0;
 
-	// integer to hold the id
+	// integer to hold the id set to 0
 	int piece_id_king = 0;
 
 
@@ -23,30 +23,28 @@ public:
 	// constructor with parameters for the owner color and the piece id
 	King(int owner, int piece_id);
 
-	// validating the king`s move
+	// overriding is_move_valid method defined in Piece.h
 	bool is_move_valid(int startRow, int startCol, int endRow, int endCol, int board[8][8]) override;
 
-	// validating the castling move
+	// method for castling moves validation
 	bool castling(int startRow, int startCol, int endRow, int endCol, int board[8][8]);
 
-
-
-	// validating the right castling squares
+	// overriding are_right_castling_sqs_valid defined in Piece.h
 	virtual bool are_right_castling_sqs_valid(int owner, int startRow, int startCol, int board[8][8]) override;
 	
-	// validating the left castling squares
+	// overriding are_left_castling_sqs_valid defined in Piece.h
 	virtual bool are_left_castling_sqs_valid(int owner, int startRow, int startCol, int board[8][8]) override;
 
-	// validating whether the right castling squares are in check
+	// method for in-check validation on the right castling squares
 	bool are_right_castling_sqs_in_check(int owner, int startRow, int startCol, int board[8][8]);
 
-	// validating whether the left castling sqares are in check
+	// method for in-check validation on the left castling squares
 	bool are_left_castling_sqs_in_check(int owner, int startRow, int startCol, int board[8][8]);
 
-	// validating the first castling square
+	// method for in-check validation on the 1st castling square
 	bool is_sq1_in_check(int owner, int side, int sqRow, int sqCol, int board[8][8]);
 	
-	// validating the second castling square
+	// method for in-check validation on the 2nd castling square
 	bool is_sq2_in_check(int owner, int side, int sqRow, int sqCol, int board[8][8]);
 
 
