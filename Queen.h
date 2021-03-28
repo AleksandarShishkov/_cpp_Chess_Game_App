1d24cd22297929bf9e5909a3b75252a42db92e4a
Aleksandar Shishkov <74405864+AleksandarShishkov@users.noreply.github.com>
2021-03-28 14:41:59 +0300

    Update Queen.h

--- Queen.h
+++ Queen.h
@@ -1,7 +1,7 @@
 #ifndef QUEEN_H
 #define QUEEN_H
 
-
+// including Piece.h
 #include "Piece.h"
 
 class Queen :public Piece
@@ -22,7 +22,7 @@ public:
 	// constructor with parameters for the owner color and the piece id
 	Queen(int owner, int piece_id);
 
-	// validating the queen`s move
+	// overriding is_move_valid method defined in Piece.h
 	bool is_move_valid(int startRow, int startCol, int endRow, int endCol, int board[8][8]) override;
 
 
