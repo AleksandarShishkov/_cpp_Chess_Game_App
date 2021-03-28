cea564b83942e8b7f21d5274b8b3149ee5dc9d53
Aleksandar Shishkov <74405864+AleksandarShishkov@users.noreply.github.com>
2021-03-28 14:40:03 +0300

    Update Bishop.h

--- Bishop.h
+++ Bishop.h
@@ -1,7 +1,7 @@
 #ifndef BISHOP_H
 #define BISHOP_H
 
-// including header file Piece.h
+// including Piece.h
 #include "Piece.h"
 
 class Bishop :public Piece
@@ -9,10 +9,10 @@ class Bishop :public Piece
 
 private:
 
-	// integer to hold the owner
+	// integer to hold the owner set to 0
 	int owner_bishop = 0;
 
-	// integer to hold the id
+	// integer to hold the id set to 0
 	int piece_id_bishop = 0;
 
 public:
@@ -20,7 +20,7 @@ public:
 	// constructor with parameters for the owner color and the piece id
 	Bishop(int owner, int piece_id);
 
-	// validating the bishop`s move
+	// overriding is_move_valid method defined in Piece.h
 	bool is_move_valid(int startRow, int startCol, int endRow, int endCol, int board[8][8]) override;
 
 
