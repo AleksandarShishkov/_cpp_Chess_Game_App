3ae5ef6e820b7426582b7fee9acde2b2a8adda7f
Alex <74405864+AleksandarShishkov@users.noreply.github.com>
2024-03-28 22:11:18 +0200

    Update Knight.cpp

--- Knight.cpp
+++ Knight.cpp
@@ -3,27 +3,22 @@
 
 Knight::Knight(int owner, int piece_id) :Piece(owner, piece_id)
 {
-	// assigning owner to owner_knight
 	this->owner_knight = owner;
 
-	// assigning piece_id to piece_id_knight
 	this->piece_id_knight = piece_id;
 }
 
 
 bool Knight::is_move_valid(int startRow, int startCol, int endRow, int endCol, int board[8][8])
 {
-	// if it is white`s turn
 	if (get_owner() == 0)
 	{
 
-		// if the destination square is valid
 		if ((board[endRow - 1][endCol - 1] >= 7 && board[endRow - 1][endCol - 1] <= 12) ||
 			board[endRow - 1][endCol - 1] == 0)
 		{
 
 
-			// validating the white knight`s upper right side
 			if ((endRow - 1 == startRow - 3) && (endCol - 1 == startCol))
 			{
 				return true;
@@ -32,7 +27,6 @@ bool Knight::is_move_valid(int startRow, int startCol, int endRow, int endCol, i
 			{
 				return true;
 			}
-			// validating the white knight`s lower right side
 			else if ((endRow - 1 == startRow) && (endCol - 1 == startCol + 1))
 			{
 				return true;
@@ -41,7 +35,6 @@ bool Knight::is_move_valid(int startRow, int startCol, int endRow, int endCol, i
 			{
 				return true;
 			}
-			// vallidating the white knight`s lower left side 
 			else if ((endRow - 1 == startRow + 1) && (endCol - 1 == startCol - 2))
 			{
 				return true;
@@ -50,7 +43,6 @@ bool Knight::is_move_valid(int startRow, int startCol, int endRow, int endCol, i
 			{
 				return true;
 			}
-			// validating the white knight`s upper left side
 			else if ((endRow - 1 == startRow - 2) && (endCol - 1 == startCol - 3))
 			{
 				return true;
@@ -63,17 +55,14 @@ bool Knight::is_move_valid(int startRow, int startCol, int endRow, int endCol, i
 		}
 
 	}
-	// if it is black`s turn
 	else if (get_owner() == 1)
 	{
 
-		// if the destination square is valid
 		if ((board[endRow - 1][endCol - 1] >= 1 && board[endRow - 1][endCol - 1] <= 6) ||
 			board[endRow - 1][endCol - 1] == 0)
 		{
 
 
-			// validating the black knight`s upper right side
 			if ((endRow - 1 == startRow - 3) && (endCol - 1 == startCol))
 			{
 				return true;
@@ -82,8 +71,6 @@ bool Knight::is_move_valid(int startRow, int startCol, int endRow, int endCol, i
 			{
 				return true;
 			}
-			// validating the black knight`s lower right side
-			else if ((endRow - 1 == startRow) && (endCol - 1 == startCol + 1))
 			{
 				return true;
 			}
@@ -91,7 +78,6 @@ bool Knight::is_move_valid(int startRow, int startCol, int endRow, int endCol, i
 			{
 				return true;
 			}
-			// vallidating the black knight`s lower left side 
 			else if ((endRow - 1 == startRow + 1) && (endCol - 1 == startCol - 2))
 			{
 				return true;
@@ -100,7 +86,6 @@ bool Knight::is_move_valid(int startRow, int startCol, int endRow, int endCol, i
 			{
 				return true;
 			}
-			// validating the black knight`s upper left side
 			else if ((endRow - 1 == startRow - 2) && (endCol - 1 == startCol - 3))
 			{
 				return true;
