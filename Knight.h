f2c75b9234d4774538adf7a7e34b1d66e0031878
Alex <74405864+AleksandarShishkov@users.noreply.github.com>
2024-03-28 22:11:43 +0200

    Update Knight.h

--- Knight.h
+++ Knight.h
@@ -1,7 +1,6 @@
 #ifndef KNIGHT_H
 #define KNIGHT_H
 
-// including Piece.h
 #include "Piece.h"
 
 class Knight :public Piece
@@ -10,18 +9,14 @@ class Knight :public Piece
 
 private:
 
-	// integer to hold the owner set to 0
 	int owner_knight = 0;
 
-	// integer to hold the id set to 0
 	int piece_id_knight = 0;
 
 public:
 
-	// constructor with parameters for the owner color and the piece id
 	Knight(int owner, int piece_id);
 
-	// overriding is_move_valid method defined in Piece.h
 	bool is_move_valid(int startRow, int startCol, int endRow, int endCol, int board[8][8]) override;
 
 
